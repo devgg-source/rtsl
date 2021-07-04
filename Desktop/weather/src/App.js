35ee5d416c89a0b3793efdf848615293100e61bc
devgg-source <ggkarthikmessi007@gmail.com>
2021-07-04 18:11:18 +0530

    Dense table created

--- Desktop/weather/src/App.js
+++ Desktop/weather/src/App.js
@@ -2,6 +2,7 @@ import Sign from "./components/Sign";
 import Navbar from "./components/Navbar";
 import Sidenav from "./components/Sidenav";
 import Table from "./components/Table";
+import DenseTable from "./components/DenseTable";
 import { ThemeProvider } from "@material-ui/core";
 import { theme } from "./components/Theme";
 
@@ -11,8 +12,9 @@ function App() {
       <div className="App">
         {/* <Sign /> */}
 
-        <Navbar />
+        {/* <Navbar /> */}
         {/* <Table /> */}
+        <DenseTable />
       </div>
     </ThemeProvider>
   );
